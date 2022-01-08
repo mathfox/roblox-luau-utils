edddb131279a23d83185430f52f29e40edfdb160
nonamie <53796817+noname156@users.noreply.github.com>
2022-01-08 08:28:41 +0300

    getPlayersInRadiusList

---
+++ src/shared/PlayersUtils/getPlayersInRadiusList.lua
@@ -0,0 +1,24 @@
+export type PlayersInRadiusList = { Player }
+
+local Players = game:GetService("Players")
+
+local function getPlayersInRadiusList(
+	position: Vector3,
+	radius: number,
+	overlapParams: OverlapParams?
+): PlayersInRadiusList
+	local playersInRadiusList: PlayersInRadiusList = {}
+
+	local basePartsFoundInRadius = workspace:GetPartBoundsInRadius(position, radius, overlapParams)
+
+	for _, basePart in ipairs(basePartsFoundInRadius) do
+		local player: Player? = Players:GetPlayerFromCharacter(basePart.Parent)
+		if player and not table.find(playersInRadiusList, player) then
+			table.insert(playersInRadiusList, player)
+		end
+	end
+
+	return playersInRadiusList
+end
+
+return getPlayersInRadiusList
---
+++ src/shared/PlayersUtils/init.lua
@@ -0,0 +1,5 @@
+local PlayersUtils = {
+	getPlayersInRadiusList = require(script.getPlayersInRadiusList),
+}
+
+return PlayersUtils
