eb8b301bb451c1ebe43030f220bb63f47ae4b3a8
nonamie <53796817+noname156@users.noreply.github.com>
2022-03-28 17:33:18 +0300

    better Queue types, posOverride support

--- src/shared/Queue/Types.lua
+++ src/shared/Queue/Types.lua
@@ -1,9 +1,9 @@
-export type Queue = {
-	enqueue: (self: Queue, ...any) -> (),
-	dequeue: (self: Queue) -> ...any,
-	getFront: (self: Queue) -> ...any,
-	getBack: (self: Queue) -> ...any,
-	getLength: (self: Queue) -> number,
+export type Queue<V...> = {
+	enqueue: (self: Queue<V...>, V...) -> (),
+	dequeue: (self: Queue<V...>, posOverride: number?) -> V...,
+	getFront: (self: Queue<V...>) -> V...,
+	getBack: (self: Queue<V...>) -> V...,
+	getLength: (self: Queue<V...>) -> number,
 }
 
 return nil
--- src/shared/Queue/init.lua
+++ src/shared/Queue/init.lua
@@ -1,14 +1,18 @@
 local Types = require(script.Types)
 
+type Queue<V...> = Types.Queue<V...>
+
 local Queue = {}
-Queue.prototype = {}
+Queue.prototype = {} :: Queue<any> & {
+	_queue: { { [number]: any, n: number } },
+}
 Queue.__index = Queue.prototype
 
 function Queue.is(object)
 	return type(object) == "table" and getmetatable(object) == Queue
 end
 
-function Queue.new(): Types.Queue
+function Queue.new(): Queue
 	return setmetatable({
 		_queue = {},
 	}, Queue)
@@ -18,22 +22,24 @@ function Queue.prototype:enqueue(...)
 	table.insert(self._queue, 1, table.pack(...))
 end
 
-function Queue.prototype:dequeue(): ...any
-	local tbl = table.remove(self._queue)
-	return table.unpack(tbl, 1, tbl.n)
+function Queue.prototype:dequeue(posOverride: number?)
+	local tbl = table.remove(self._queue, posOverride)
+	return unpack(tbl, 1, tbl.n)
 end
 
 function Queue.prototype:getFront(): ...any
-	local queue = self._queue
-	return table.unpack(queue[#queue])
+	return unpack(self._queue[#self._queue])
 end
 
 function Queue.prototype:getBack(): ...any
-	return table.unpack(self._queue[1])
+	return unpack(self._queue[1])
 end
 
-function Queue.prototype:getLength(): number
+function Queue.prototype:getLength()
 	return #self._queue
 end
 
-return Queue
+return Queue :: {
+	is: (object: any) -> boolean,
+	new: <V...>() -> Queue<V...>,
+}
