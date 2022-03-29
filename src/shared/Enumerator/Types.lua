2b4d1b12d7eaf6f87eb2a06c25fe10d5f7388d52
nonamie <53796817+noname156@users.noreply.github.com>
2022-03-29 10:43:14 +0300

    fix Enumerator type

--- src/shared/Enumerator/Types.lua
+++ src/shared/Enumerator/Types.lua
@@ -3,7 +3,6 @@ export type EnumeratorItem<V, K> = { name: K, type: Enumerator<V, K>, value: V }
 export type Enumerator<V, K> = {
 	fromRawValue: (rawValue: V) -> EnumeratorItem<V, K>?,
 	isEnumValue: (value: any) -> boolean,
-	cast: (value: any) -> (EnumeratorItem<V, K> | boolean, string?),
 	getEnumeratorItems: () -> { EnumeratorItem<V, K> },
 }
 
