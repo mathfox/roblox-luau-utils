a79087d42bbb9e6f20b8417e732e236f3552f457
nonamie <53796817+noname156@users.noreply.github.com>
2022-04-12 02:59:25 +0300

    updated tests, refactor project structures

--- tests/runner.server.lua
+++ tests/runner.server.lua
@@ -1,13 +1,4 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local TestService = game:GetService("TestService")
-
 local TestEZ = require(ReplicatedStorage:WaitForChild("TestEZ"))
 
-TestEZ.TestBootstrap:run({ game:GetService("StarterPlayer"), ReplicatedStorage, game:GetService("ServerStorage") }, {
-	report = function(results)
-		for _, message in ipairs(results.errors) do
-			TestService:Error(message)
-			print("")
-		end
-	end,
-})
+TestEZ.TestBootstrap:run({ game:GetService("StarterPlayer"), ReplicatedStorage, game:GetService("ServerStorage") })
