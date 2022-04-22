d1af0217a250222d06be825a5a1d95900efd0e68
nonamie <53796817+noname156@users.noreply.github.com>
2022-04-22 16:17:41 +0300

    Update init.spec.lua

--- src/shared/FunctionUtils/returnArgs/init.spec.lua
+++ src/shared/FunctionUtils/returnArgs/init.spec.lua
@@ -4,7 +4,7 @@ return function()
 	it("should provide proper values", function()
 		expect(function()
 			local a, b, c = returnArgs(1, 2, 3)()
-			assert(a == 1, b == 2, c == 3)
+			assert(a == 1 and b == 2 and c == 3)
 		end).never.throw()
 	end)
 end
