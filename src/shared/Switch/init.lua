1e97404708d87d05f1e8bab27e7e926ed8bbb609
nonamie <53796817+noname156@users.noreply.github.com>
2022-04-24 19:21:37 +0300

    fix switch util name

--- src/shared/Switch/init.lua
+++
@@ -1,6 +0,0 @@
-local function switch(condition, results: { [any]: any, default: any })
-	local exists = results[condition] or results.default
-	return if type(exists) == "function" then exists() else exists
-end
-
-return switch
