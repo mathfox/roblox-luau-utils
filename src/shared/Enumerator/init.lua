520318f872277619f5c773f65845ce326aa6d8b8
nonamie <53796817+noname156@users.noreply.github.com>
2022-05-17 20:19:04 +0300

    refactor Enumerator

--- src/shared/Enumerator/init.lua
+++ src/shared/Enumerator/init.lua
@@ -1,11 +1,12 @@
--- source: https://github.com/howmanysmall/enumerator
+-- refactored version of: https://github.com/howmanysmall/enumerator
 
 local Types = require(script.Parent.Types)
 
-export type EnumeratorItem<T> = Types.EnumeratorItem<T>
-export type Enumerator<T> = Types.Enumerator<T>
 type Record<K, V> = Types.Record<K, V>
 type Array<T> = Types.Array<T>
+
+export type EnumeratorItem<T> = Types.EnumeratorItem<T>
+export type Enumerator<T> = Types.Enumerator<T>
 
 local function outputHelper(...)
 	local length = select("#", ...)
@@ -19,35 +20,96 @@ local function outputHelper(...)
 	return table.concat(tbl, ", ")
 end
 
-local function Enumerator<T>(enumeratorName: string, enumValues: Array<string> | Record<string, T>, ...): Enumerator<T>
+local function EnumeratorConstructor<T>(enumeratorName: string, enumValues: Array<string> | Record<string, T>, ...)
 	if type(enumeratorName) ~= "string" then
 		error(('"enumeratorName" (#1 argument) must be a string, got (%s) instead'):format(outputHelper(enumeratorName)), 2)
 	elseif enumeratorName == "" then
 		error('"enumeratorName" (#1 argument) must be a non-empty string', 2)
 	elseif type(enumValues) ~= "table" then
-		error(('"enumValues" (#2 argument) must be either an Array<string> or Record<string, T>, got (%s) instead'):format(outputHelper(enumValues)), 2)
+		error(('"enumValues" (#2 argument) must be either an Array<string> or a Record<string, T>, got (%s) instead'):format(outputHelper(enumValues)), 2)
 	elseif select("#", ...) > 0 then
 		error(('"Enumerator" constructor expects exactly two arguments: (string, Array<string> | Record<string, T>), got (%s) as well'):format(outputHelper(...)), 2)
 	end
 
-	-- first look which decides the table type other values will be asserted against
-	-- we are only interested in assigning Array, as otherwise we suppose it must be a Dictionary
+	-- this variable decides type string other key types will be asserted against
 	local firstKeyType = type(next(enumValues))
 
-	if firstKeyType == nil then
-		error('empty "enumValues" (#2 argument) table provided, expected either Array<string> or Record<string, T>', 2)
+	if firstKeyType == "nil" then
+		error('empty "enumValues" (#2 argument) table provided, expected either an Array<string> or a Record<string, T> with atleast two entries in it', 2)
 	elseif firstKeyType ~= "number" and firstKeyType ~= "string" then
-		error(('invalid "enumValues" (#2 argument) provided, expected either Array<string> or Record<string, T> but got key of (%s) type instead'):format(firstKeyType), 2)
+		error(
+			(
+				'invalid key of (%s) type specified in provided "enumValues" (#2 argument) table which should be either an Array<string> or a Record<string, T> (so the key type could only be either a number or a string)'
+			):format(typeof(next(enumValues))),
+			2
+		)
+	elseif getmetatable(enumValues) then
+		error(
+			('"enumValues" (#2 argument) table whose type presumably is %s, should not contain a metatable attached to it'):format(
+				if firstKeyType == "number" then "an Array<string>" else ("a Record<string, %s>"):format(typeof(select(2, next(enumValues))))
+			)
+		)
 	end
 
-	local tblType = if firstKeyType == "number" then "Array" else nil
+	local enumerator = {}
+	local keysToEnumeratorItems: Record<string, EnumeratorItem<T>> = {}
+	local valuesToEnumeratorItems: Record<T, EnumeratorItem<T>> = {}
+	local totalEnumeratorItems = 0
 
-	for key, value in pairs(enumValues) do
-		local keyType = type(key)
+	if firstKeyType == "number" then
+		for index, value in pairs(enumValues :: Array<string>) do
+			if type(index) ~= "number" then
+				error(('invalid key specified in "enumValues" (#2 argument) table which presumably is an Array<string>, expected a number but got (%s) instead'):format(outputHelper(index)), 2)
+			end
 
-		if tblType == "Array" then
-			if keyType ~= "number" then
-				error(('invalid key provided in "enumValues": Array<string> (#2 argument), expected number but got (%s) instead'):format(outputHelper(keyType)), 2)
+			totalEnumeratorItems += 1
+
+			if index ~= totalEnumeratorItems then
+				local missingIndexes: Array<number> = { totalEnumeratorItems }
+				local currentMissingIndex = totalEnumeratorItems
+
+				-- will be used in case there is also an invalid keys provided
+				local invalidKeys: Array<any> = nil
+
+				for nextIndex in next, enumValues, index do
+					currentMissingIndex += 1
+
+					if type(nextIndex) == "number" then
+						local existingIndex = table.find(missingIndexes, nextIndex)
+						if existingIndex then
+							table.remove(missingIndexes, existingIndex)
+						end
+
+						if nextIndex > index then
+							for i = index + 1, nextIndex - 1 do
+								if i ~= index and not table.find(missingIndexes, i) then
+									table.insert(missingIndexes, i)
+								end
+							end
+						else
+							for i = index - 1, nextIndex + 1, -1 do
+								if not table.find(missingIndexes, i) then
+									table.insert(missingIndexes, i)
+								end
+							end
+						end
+					elseif not invalidKeys then
+						invalidKeys = { nextIndex }
+					else
+						table.insert(invalidKeys, nextIndex)
+					end
+				end
+
+				table.sort(missingIndexes)
+
+				error(
+					('"enumValues" (#2 argument) table which type presumably is an Array<string> contains (%s) missing index%s%s'):format(
+						table.concat(missingIndexes, ", "),
+						if #missingIndexes == 1 then "" else "es",
+						if invalidKeys then (" as well as invalid keys (%s)"):format(outputHelper(unpack(invalidKeys, 1, #invalidKeys))) else ""
+					),
+					2
+				)
 			end
 
 			local valueType = type(value)
@@ -56,19 +118,61 @@ local function Enumerator<T>(enumeratorName: string, enumValues: Array<string> |
 			elseif value == "" then
 				error('empty string provided as a value in "enumValues": Array<string> (#2 argument), expected non-empty string', 2)
 			end
-		else
-			if keyType == "number" then
-				error(('invalid key "%d" provided in "enumValues": Record<string, T> (#2 argument), expected string but got ("%s": number) instead'):format(key), 2)
+
+			local enumeratorItem = table.freeze(setmetatable(
+				{ name = value, type = enumerator, value = value },
+				table.freeze({
+					__tostring = function()
+						return ("%s.%s"):format(enumeratorName, value)
+					end,
+				})
+			)) :: EnumeratorItem<string>
+
+			keysToEnumeratorItems[value] = enumeratorItem
+			valuesToEnumeratorItems[value] = enumeratorItem
+		end
+	else
+		local firstValueType = typeof(select(2, next(enumValues)))
+
+		for key, value in pairs(enumValues :: Record<string, T>) do
+			if type(key) ~= "string" then
+				error(
+					('invalid key specified in "enumValues" (#2 argument) whose type is presumably is a Record<string, %s>, expected a string but got (%s) instead'):format(
+						firstValueType,
+						outputHelper(key)
+					),
+					2
+				)
 			elseif key == "" then
-				error('empty string provided as a key in "enumValues": Record<string, T> (#2 argument), expected non-empty string', 2)
+				error(('empty string provided as a key in "enumValues" (#2 argument) which is presumably is a Record<string, %s>, expected a non-empty string'):format(firstValueType), 2)
 			end
+
+			local valueType = typeof(value)
+			if valueType ~= firstValueType then
+				error(
+					('"enumValues" (#2 argument) table which is presumably is a Record<string, %s> contains invalid value (%s), expected values of (%s) type'):format(
+						firstValueType,
+						outputHelper(value),
+						firstValueType
+					),
+					2
+				)
+			end
+
+			local enumeratorItem = table.freeze(setmetatable(
+				{ name = key, type = enumerator, value = value },
+				table.freeze({
+					__tostring = function()
+						return ("%s.%s"):format(enumeratorName, key)
+					end,
+				})
+			)) :: EnumeratorItem<T>
+
+			keysToEnumeratorItems[key] = enumeratorItem
+			valuesToEnumeratorItems[value] = enumeratorItem
+			totalEnumeratorItems += 1
 		end
 	end
-
-	local enumerator = {}
-	local keysToEnumeratorItems: Record<string, EnumeratorItem<T>> = {}
-	local valuesToEnumeratorItems: Record<T, EnumeratorItem<T>> = {}
-	local totalEnums = 0
 
 	function enumerator.fromRawValue(rawValue): EnumeratorItem<T>?
 		return valuesToEnumeratorItems[rawValue]
@@ -88,8 +192,8 @@ local function Enumerator<T>(enumeratorName: string, enumValues: Array<string> |
 		return false
 	end
 
-	function enumerator.getEnumeratorItems(): { EnumeratorItem<T> }
-		local enumeratorItems = table.create(totalEnums)
+	function enumerator.getEnumeratorItems()
+		local enumeratorItems = table.create(totalEnumeratorItems) :: Array<EnumeratorItem<T>>
 		local length = 0
 
 		for _, enumeratorItem in pairs(keysToEnumeratorItems) do
@@ -100,47 +204,7 @@ local function Enumerator<T>(enumeratorName: string, enumValues: Array<string> |
 		return enumeratorItems
 	end
 
-	if tblType == "Array" then
-		for _, valueName in ipairs(enumValues :: Array<string>) do
-			local enumeratorItem = table.freeze(setmetatable(
-				{
-					name = valueName,
-					type = enumerator,
-					value = valueName,
-				},
-				table.freeze({
-					__tostring = function()
-						return ("%s.%s"):format(enumeratorName, valueName)
-					end,
-				})
-			))
-
-			keysToEnumeratorItems[valueName] = enumeratorItem
-			valuesToEnumeratorItems[valueName] = enumeratorItem
-			totalEnums += 1
-		end
-	else
-		for valueName, rawValue in pairs(enumValues :: Record<string, T>) do
-			local enumeratorItem = table.freeze(setmetatable(
-				{
-					name = valueName,
-					type = enumerator,
-					value = rawValue,
-				},
-				table.freeze({
-					__tostring = function()
-						return ("%s.%s"):format(enumeratorName, valueName)
-					end,
-				})
-			))
-
-			keysToEnumeratorItems[valueName] = enumeratorItem
-			valuesToEnumeratorItems[rawValue] = enumeratorItem
-			totalEnums += 1
-		end
-	end
-
-	table.freeze(setmetatable(
+	return table.freeze(setmetatable(
 		enumerator,
 		table.freeze({
 			__index = keysToEnumeratorItems,
@@ -148,9 +212,7 @@ local function Enumerator<T>(enumeratorName: string, enumValues: Array<string> |
 				return enumeratorName
 			end,
 		})
-	))
-
-	return enumerator
+	)) :: Enumerator<T>
 end
 
-return Enumerator
+return EnumeratorConstructor
