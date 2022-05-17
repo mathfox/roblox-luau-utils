aa9a1b6a2bc49107d25794101fd964490fd4d3e5
nonamie <53796817+noname156@users.noreply.github.com>
2022-05-17 15:34:58 +0300

    Update init.lua

--- src/shared/TableUtils/random/init.lua
+++ src/shared/TableUtils/random/init.lua
@@ -3,7 +3,8 @@ local Types = require(script.Parent.Parent.Types)
 type Array<T> = Types.Array<T>
 
 local function random<V>(arr: Array<V>, rngOverride: Random?)
-	return arr[(rngOverride or Random.new()):NextInteger(1, #arr)]
+	local length = #arr
+	return if length == 1 then arr[1] else arr[(rngOverride or Random.new()):NextInteger(1, length)]
 end
 
 return random
