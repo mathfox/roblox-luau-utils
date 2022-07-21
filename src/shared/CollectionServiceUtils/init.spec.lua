6db86689d8212104b7cf172c266ab057620322a0
nonamie <53796817+noname156@users.noreply.github.com>
2022-07-21 10:18:39 +0300

    add CollectionServiceUtils expose api test

--- src/shared/CollectionServiceUtils/init.spec.lua
+++ src/shared/CollectionServiceUtils/init.spec.lua
@@ -18,4 +18,17 @@ return function()
 			setmetatable(CollectionServiceUtils, {})
 		end).to.throw()
 	end)
+
+	it("should expose only known apis", function()
+		local APIS = {
+			findFirstAncestorOfTag = "function",
+			hasTags = "function",
+			removeAllTags = "function",
+		}
+
+		for key, value in CollectionServiceUtils do
+			assert(APIS[key] ~= nil)
+			assert(type(value) == APIS[key])
+		end
+	end)
 end
