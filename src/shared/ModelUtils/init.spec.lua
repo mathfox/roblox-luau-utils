734d9c009dc71bf5817ddb7da7ae15e3402eaf9c
nonamie <53796817+noname156@users.noreply.github.com>
2022-07-25 13:11:15 +0300

    add more tests

--- src/shared/ModelUtils/init.spec.lua
+++ src/shared/ModelUtils/init.spec.lua
@@ -1,6 +1,14 @@
 return function()
 	local ModelUtils = require(script.Parent)
 
+	it("should be a table", function()
+		expect(ModelUtils).to.be.a("table")
+	end)
+
+	it("should not contain a metatable", function()
+		expect(getmetatable(ModelUtils)).to.equal(nil)
+	end)
+
 	it("should throw an error on attempt to modify the export table", function()
 		expect(function()
 			ModelUtils.NEW_FIELD = {}
